f47d1c000e3bee15e5db0164fc2c2335a8135ad6
carlos <bougar@gmx.es>
2016-04-27 02:04:01 +0200

    test

--- include/Cxt001Pass.h
+++ include/Cxt001Pass.h
@@ -1,7 +1,9 @@
 #include "FunctionInfo.h"
 #include "llvm/Pass.h"
+#include "llvm/ADT/Triple.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Function.h"
+#include "llvm/Analysis/TargetLibraryInfo.h"
 #include <tuple>
 using namespace std;
 
@@ -9,7 +11,11 @@ class Cxt001Pass : public llvm::FunctionPass {
 public:
   static char ID;
 
-	Cxt001Pass() : llvm::FunctionPass(ID),funCounter(1) {};
+	Cxt001Pass(const llvm::Module * m) : llvm::FunctionPass(ID),funCounter(1) {
+		module = m; 
+		llvm::Twine twine=llvm::Twine(m->getTargetTriple());
+		triple = llvm::Triple(twine);
+	};
 
 
   void getAnalysisUsage(llvm::AnalysisUsage &AU) const override;
@@ -21,6 +27,8 @@ public:
 
 	
 	private:
+		const llvm::Module * module;
+		llvm::Triple triple;
 		vector<FunctionInfo> functionOperationsVector;	
 		int funCounter;
 		vector<Memoria> memoryVector;
